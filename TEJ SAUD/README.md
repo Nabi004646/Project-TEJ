de4c9b0730609db89c5d6e0ee12c7c0a5a5d1f16
React_Boy⚡ <sudosuWz61m58x004646@gmail.com>
2025-03-15 21:57:31 +0545

    Tej Sir Portfolio Uploaded

---
+++ TEJ SAUD/README.md
@@ -0,0 +1,18 @@
+# Responsive Portfolio Website 
+
+Website Using HTML, CSS, SCSS and JavaScript, with a wonderful user interface.
+Author : Nabin Kunwar
+<br>
+Developed for Tej saud
+ 
+## Website contains: 
+
+- Home
+- About Me
+- Skills
+- Qualification
+- Portfolio
+- Game (Simple Ludo)
+- Contact
+
+<p align="center"><b>© Created by Nabin Kunwar</b></p?
---
+++ TEJ SAUD/app.js
@@ -0,0 +1,55 @@
+//Author : Nabin Kunwar
+
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+
+function PageTransition(){
+    //Button click active class
+    for(let i=0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function() {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+    
+    //sections active class
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from the other btns
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other section
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+
+    //Toogle theme
+
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode');
+    })
+
+}
+
+PageTransition();
+
+// AUTHOR IDENTIFICATION
+document.addEventListener('copy',function(event){
+    event.clipboardData.setData('text/plain','Real Author : Nabin Kunwar (React Boys) [ Client : Tej bahadur saud ]');
+    event.preventDefault();
+});
